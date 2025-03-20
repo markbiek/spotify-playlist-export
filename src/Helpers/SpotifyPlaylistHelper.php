fbbfca413ee5d4de2fa4823f01e732217191bb78
Mark Biek <mark.biek@automattic.com>
2025-03-20 13:43:30 -0400

    Function to get all playlist tracks

--- src/Helpers/SpotifyPlaylistHelper.php
+++ src/Helpers/SpotifyPlaylistHelper.php
@@ -47,4 +47,26 @@ class SpotifyPlaylistHelper {
 
 		return $playlists;
 	}
+
+	/**
+	 * Get all tracks from a playlist.
+	 *
+	 * @param string $playlistId The Spotify playlist ID.
+	 * @return array The playlist tracks.
+	 */
+	public function getPlaylistTracks(string $playlistId): array {
+		$tracks = [];
+		$options = [
+			'limit' => 50, // Maximum allowed by the API
+			'offset' => 0,
+		];
+
+		do {
+			$response = $this->api->getPlaylistTracks($playlistId, $options);
+			$tracks = array_merge($tracks, $response->items);
+			$options['offset'] += $options['limit'];
+		} while (count($tracks) < $response->total);
+
+		return $tracks;
+	}
 } 
--- tests/Unit/Helpers/SpotifyPlaylistHelperTest.php
+++ tests/Unit/Helpers/SpotifyPlaylistHelperTest.php
@@ -86,4 +86,100 @@ class SpotifyPlaylistHelperTest extends TestCase {
 		$this->assertEmpty($result);
 		$this->assertIsArray($result);
 	}
+
+	/**
+	 * Test retrieving tracks from a single-page playlist.
+	 */
+	public function testGetPlaylistTracksWithSinglePage(): void {
+		$playlistId = 'test_playlist_id';
+		$mockTracks = (object) [
+			'items' => [
+				(object) ['track' => (object) ['name' => 'Track 1']],
+				(object) ['track' => (object) ['name' => 'Track 2']],
+			],
+			'total' => 2
+		];
+
+		$this->mockApi->expects($this->once())
+			->method('getPlaylistTracks')
+			->with(
+				$this->equalTo($playlistId),
+				$this->equalTo(['limit' => 50, 'offset' => 0])
+			)
+			->willReturn($mockTracks);
+
+		$result = $this->helper->getPlaylistTracks($playlistId);
+
+		$this->assertCount(2, $result);
+		$this->assertEquals('Track 1', $result[0]->track->name);
+		$this->assertEquals('Track 2', $result[1]->track->name);
+	}
+
+	/**
+	 * Test retrieving tracks from a multi-page playlist.
+	 */
+	public function testGetPlaylistTracksWithMultiplePages(): void {
+		$playlistId = 'test_playlist_id';
+		$page1 = (object) [
+			'items' => [
+				(object) ['track' => (object) ['name' => 'Track 1']],
+				(object) ['track' => (object) ['name' => 'Track 2']],
+			],
+			'total' => 3
+		];
+
+		$page2 = (object) [
+			'items' => [
+				(object) ['track' => (object) ['name' => 'Track 3']],
+			],
+			'total' => 3
+		];
+
+		$expectedCalls = [
+			['offset' => 0, 'response' => $page1],
+			['offset' => 50, 'response' => $page2]
+		];
+		$callCount = 0;
+
+		$this->mockApi->expects($this->exactly(2))
+			->method('getPlaylistTracks')
+			->willReturnCallback(function($id, $options) use ($playlistId, $expectedCalls, &$callCount) {
+				$this->assertEquals($playlistId, $id);
+				$this->assertEquals($expectedCalls[$callCount]['offset'], $options['offset']);
+				$this->assertEquals(50, $options['limit']);
+				
+				return $expectedCalls[$callCount++]['response'];
+			});
+
+		$result = $this->helper->getPlaylistTracks($playlistId);
+
+		$this->assertCount(3, $result);
+		$this->assertEquals('Track 1', $result[0]->track->name);
+		$this->assertEquals('Track 2', $result[1]->track->name);
+		$this->assertEquals('Track 3', $result[2]->track->name);
+	}
+
+	/**
+	 * Test retrieving tracks from an empty playlist.
+	 */
+	public function testGetPlaylistTracksWithEmptyPlaylist(): void {
+		$playlistId = 'test_playlist_id';
+		$mockTracks = (object) [
+			'items' => [],
+			'total' => 0
+		];
+
+		$this->mockApi->expects($this->once())
+			->method('getPlaylistTracks')
+			->with(
+				$this->equalTo($playlistId),
+				$this->equalTo(['limit' => 50, 'offset' => 0])
+			)
+			->willReturn($mockTracks);
+
+		$result = $this->helper->getPlaylistTracks($playlistId);
+
+		$this->assertEmpty($result);
+		$this->assertIsArray($result);
+	}
 } 
