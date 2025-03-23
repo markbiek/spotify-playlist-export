e68a145b44acef547fe9425c4b672a98e2caeabc
Mark Biek <mark.biek@automattic.com>
2025-03-23 16:04:59 -0400

    Class for tracking a playlist export

---
+++ app/Models/SpotifyPlaylistExport.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class SpotifyPlaylistExport extends Model
+{
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<string>
+	 */
+	protected $fillable = [
+		'user_id',
+		'finished',
+		'playlist_count',
+		'playlists_exported',
+	];
+
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array<string, string>
+	 */
+	protected $casts = [
+		'finished' => 'boolean',
+		'playlist_count' => 'integer',
+		'playlists_exported' => 'integer',
+	];
+
+	/**
+	 * Get the user that owns the export.
+	 */
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+}
